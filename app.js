54f834fc6b1392904d7d7736a524d503a1686c28
maggielau <maggie.my.lau@gmail.com>
2022-07-05 22:41:39 -0400

    Corrected start script

--- app.js
+++ app.js
@@ -44,8 +44,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3001, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+// const port = process.env.PORT || 3001;
+// app.listen(port, () => {
+//   console.log(`Server started on port ${port}`);
+// });
 
 module.exports = app;
